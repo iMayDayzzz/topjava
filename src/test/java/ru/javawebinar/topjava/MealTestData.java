3a230ef823e043a2559649b44c15b54a786636e1
Victor <iimaydaysii@gmail.com>
2020-07-01 16:52:30 +0300

    apply patch fix

---
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -0,0 +1,19 @@
+package ru.javawebinar.topjava;
+
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
+
+public class MealTestData {
+
+
+
+}
